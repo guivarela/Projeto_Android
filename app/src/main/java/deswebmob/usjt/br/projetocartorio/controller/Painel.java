9650a08a9484040acc82bdf35a78b841e2d9534d
Guilherme Varela <guilherme.varela@foster.com.br>
2018-04-26 14:38:48 -0300

    activity painel criado(sem funcionalidades)

---
+++ app/src/main/java/deswebmob/usjt/br/projetocartorio/controller/Painel.java
@@ -0,0 +1,16 @@
+package deswebmob.usjt.br.projetocartorio.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import deswebmob.usjt.br.projetocartorio.R;
+
+public class Painel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_painel);
+    }
+}
+
